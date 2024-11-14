55320bd0b02996e517f5072e916285fb0a4d327a
Ari Yellin-Levine <arilev65@gmail.com>
2024-11-14 13:12:00 -0500

    made cleaner

--- src/App.java
+++ src/App.java
@@ -45,19 +45,17 @@ public class App {
     }
 
     public static int firstTwoSum(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        } else if (nums.length == 1) {
-            return nums[0];
-        }
-
-        return nums[0] + nums[1];
-
+        switch (nums.length) {
+            case 0:
+                return 0;
+            case 1:
+                return nums[0];
+            default:
+                return nums[0] + nums[1];        }
     }
 
     public static int minWordLength(String sentence) {
         String[] pieces = sentence.split(" ");
-
         int shortest = 999;
 
         for (int i = 0; i < pieces.length; i++) {
